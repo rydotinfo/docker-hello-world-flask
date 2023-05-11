3c380c61184c01e904611132793ac26c143855c0
rydotinfo <126853400+rydotinfo@users.noreply.github.com>
2023-05-11 18:42:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hello, world! \n welcome to rydot Azure devops cloud. \n"
+MESSAGE = "Hello, world! \n welcome to rydot Azure dev. \n"
 
 app = Flask(__name__)
 
