1bf99877773cebf895dd3dc214660f325ed9832e
rydotinfo <126853400+rydotinfo@users.noreply.github.com>
2023-05-12 12:10:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -17,12 +17,5 @@ else:
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def root():
-    result = MESSAGE.encode("utf-8")
-    return result
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
