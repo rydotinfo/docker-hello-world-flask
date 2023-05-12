3766612ecdc3c9e2cfa9f89a6fdf831faf64b2fe
rydotinfo <126853400+rydotinfo@users.noreply.github.com>
2023-05-12 11:13:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hello, world! \n welcome to rydot Tec... \n"
+MESSAGE = "Hello, world! \n welcome to Rydot Devops Team.... \n"
 
 app = Flask(__name__)
 
