8290b9b30f57101edf815a3b69b6549a7bb448e4
rydotinfo <126853400+rydotinfo@users.noreply.github.com>
2023-05-12 12:32:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,20 +2,22 @@ from flask import Flask
 
 PORT = 8000
 
-if (2023 % 400 == 0) and (2023 % 100 == 0):
-    print("{0} is a leap year".format(2023))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (2023 % 4 ==0) and (2023 % 100 != 0):
-    print("{0} is a leap year".format(2023))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(2023))
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
+ 
+# Adding two nos
+sum = num1 + num2
+ 
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
 
 app = Flask(__name__)
+
+@app.route("/")
+def root():
+    result = MESSAGE.encode("utf-8")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
