391793565f37a31809f8f6bcc3c393f33621762f
rydotinfo <126853400+rydotinfo@users.noreply.github.com>
2023-05-12 12:01:30 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,23 +2,17 @@ from flask import Flask
 
 PORT = 8000
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+# Adding two nos
+sum = num1 + num2
 
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
 
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+
 app = Flask(__name__)
 
 
